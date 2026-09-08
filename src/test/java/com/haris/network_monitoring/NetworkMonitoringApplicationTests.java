3f767c370ace760b30312eb8541a05fbe3e30d46
Haris Granas <granasharis@gmail.com>
2026-09-08 10:21:56 +0300

    Initial commit

---
+++ src/test/java/com/haris/network_monitoring/NetworkMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haris.network_monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetworkMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
